1c680e137d08a395a0e27dc1c3c739863970e1a6
Movil <Movil@SG-POR442399DEL>
2026-04-17 19:01:54 -0600

    commit final de proyecto

---
+++ Lab4_Productos_[C00888]/Controllers/ProductosController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab4_Productos__C00888.Models;
+
+namespace Lab4_Productos__C00888.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: /Productos
+        public IActionResult Index()
+        {
+            var productos = ProductoRepositorio.ObtenerTodos();
+            return View(productos);
+        }
+
+        // GET: /Productos/Detalles/1
+        public IActionResult Detalles(int id)
+        {
+            var producto = ProductoRepositorio.ObtenerPorId(id);
+            if (producto == null)
+                return NotFound();
+
+            return View(producto);
+        }
+
+        // GET: /Productos/Crear
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        // POST: /Productos/Crear
+        [HttpPost]
+        public IActionResult Crear(Producto p)
+        {
+            if (!ModelState.IsValid)
+                return View(p);
+
+            ProductoRepositorio.Agregar(p);
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Productos/Editar/1
+        public IActionResult Editar(int id)
+        {
+            var producto = ProductoRepositorio.ObtenerPorId(id);
+            if (producto == null)
+                return NotFound();
+
+            return View(producto);
+        }
+
+        // POST: /Productos/Editar
+        [HttpPost]
+        public IActionResult Editar(Producto p)
+        {
+            if (!ModelState.IsValid)
+                return View(p);
+
+            ProductoRepositorio.Actualizar(p);
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Productos/Eliminar/1
+        public IActionResult Eliminar(int id)
+        {
+            var producto = ProductoRepositorio.ObtenerPorId(id);
+            if (producto == null)
+                return NotFound();
+
+            return View(producto);
+        }
+
+        // POST: /Productos/Eliminar/1
+        [HttpPost]
+        public IActionResult EliminarConfirmado(int id)
+        {
+            ProductoRepositorio.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Lab4_Productos_[C00888]/Models/ProductoRepositorio.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab4_Productos__C00888.Models
+{
+    public static class ProductoRepositorio
+    {
+        private static List<Producto> productos = new List<Producto>();
+        private static int nextId = 1;
+
+        public static List<Producto> ObtenerTodos()
+        {
+            return productos;
+        }
+
+        public static Producto ObtenerPorId(int id)
+        {
+            return productos.FirstOrDefault(p => p.Id == id);
+        }
+
+        public static void Agregar(Producto p)
+        {
+            p.Id = nextId++;
+            productos.Add(p);
+        }
+
+        public static void Actualizar(Producto p)
+        {
+            var productoExistente = ObtenerPorId(p.Id);
+            if (productoExistente != null)
+            {
+                productoExistente.Nombre = p.Nombre;
+                productoExistente.Precio = p.Precio;
+                productoExistente.Categoria = p.Categoria;
+            }
+        }
+
+        public static void Eliminar(int id)
+        {
+            var producto = ObtenerPorId(id);
+            if (producto != null)
+            {
+                productos.Remove(producto);
+            }
+        }
+    }
+}
